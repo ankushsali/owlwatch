9ca661bf9cf895f4d618f82d5996d1eb92100557
ankushsali <87827245+ankushsali@users.noreply.github.com>
2021-08-08 15:52:07 +0530

    Update UsersController.php

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -143,7 +143,7 @@ class UsersController extends Controller
 		$school_users = SchoolUsers::where('school_id', $request->school_id)->pluck('user_id')->toArray();
 
 		if (sizeof($school_users) > 0) {
-			$all_users = []
+			$all_users = [];
 			$ST = [];
 			$SC = [];
 			$CL = [];
